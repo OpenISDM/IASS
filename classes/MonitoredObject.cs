06451f24449e8925034d105c1658c9a9a7fbce30
Kevin Chi <kevinjipotw@gmail.com>
2015-07-03 15:39:28 +0800

    sample data

---
+++ classes/MonitoredObject.cs
@@ -0,0 +1,44 @@
+
+
+
+
+public class MonitoredObject
+{
+
+	private int _currentValue;
+	private int _validTime;
+	private int _lastUpdateTime;
+	private int _nextUpdateTime;
+
+
+	//TODO
+	//pointer to moniter conditions
+
+
+	public void AccessPath(int validTime, lastUpdateTime, nextUpdateTime)
+	{
+
+
+
+
+	}
+
+	public void UpdateMonitoredObject(int validTime, lastUpdateTime, nextUpdateTime)
+	{
+		_validTime = validTime;
+		_lastUpdateTime = lastUpdateTime;
+		_nextUpdateTime = nextUpdateTime;
+
+	}
+
+
+
+
+
+
+
+
+}
+
+
+
