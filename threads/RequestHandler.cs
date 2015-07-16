e8ba6cbdb12a887cc57b8f31919961cf8aac9149
Rimi Chen <rimi7609@gmail.com>
2015-07-16 17:38:07 +0800

    7/16 done part of threading
    7/16 done part of threading

--- threads/RequestHandler.cs
+++ threads/RequestHandler.cs
@@ -14,6 +14,9 @@ namespace IASS_Test
 
         public RequestHandler(EventWaitHandle doneSignal, EventWaitHandle waitProcessRequest)
         {
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestHandler.cs: request handler is constructed.");
+
             this._doneSignal = doneSignal;
             this._waitProcessRequest = waitProcessRequest;
         }
@@ -25,13 +28,34 @@ namespace IASS_Test
             MonitoredCondition _processedMonitoredCondtion = null;
             string pointerToMC = "";
 
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestHandler.cs: requestListener thread started.");
+
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestHandler.cs: start initialization.");
+            //initilization
+            // when initialization finished, set doneEvent
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestHandler.cs: set doneEvent[1] to say initialization finished.");
+
+            _doneSignal.Set();
+            Thread.Sleep(1000);
+            _doneSignal.WaitOne();
+
+            
+
             //while the program is not terminated
             while (!Program.isProgramExit)
             {
+                //@@@@@Debug message:
+                //Console.WriteLine("This is requestHandler.cs: while the program is not terminated.");
+
                 //if the requestProcessQueue is empty, the thread sleeps
                 if (Program.requestProcessQueue.Count == 0)
                 {
                     //use event wait handle to 
+                    //@@@@@Debug message:
+                    //Console.WriteLine("This is requestHandler.cs: block, because the queue is empty");
                     _waitProcessRequest.WaitOne();
                 }
                 else
@@ -42,23 +66,36 @@ namespace IASS_Test
                         //get the first item from request process queue
                         _currentRequest = GetRequestFromQueue();
                         //Parse the MC from the request
+
                         _processedMonitoredCondtion = ParseMC(_currentRequest.GetMCstring());
+                        //@@@@@Debug message:
+                        Console.WriteLine("***This is requestHandler.cs: reqest {0} is parsed", _currentRequest.GetRequestUID());
+
 
                         //Add processed monitored condition to MCList
                         //check if the MC has already existed: if it is,return pointer of MC; if it is not, add to MCList and return tha pointer of MC
                         pointerToMC = AddToMCList(_processedMonitoredCondtion);
+                        //@@@@@Debug message:
+                        Console.WriteLine("***This is requestHandler.cs: the MC is {0}", pointerToMC);
                         //update the request with MC pointer
                         //Add request to request list.
+
                         AddToRequestList(pointerToMC, _currentRequest);
 
+                        pointerToMC = AddToMCList(_processedMonitoredCondtion);
+
+/*
                         //schedule next update time for used monitored objects
                         //update timer table, and sort timer table by next update time
                         updateTimerTable(_processedMonitoredCondtion);
-                        
+*/                      
 
                     }
                 }
+
             }
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestHandler.cs: requestHandler thread terminated.");
         }
         public Request GetRequestFromQueue()
         {
@@ -69,6 +106,13 @@ namespace IASS_Test
         public MonitoredCondition ParseMC(string MCstring)
         {
             MonitoredCondition processedMonitoredCondtion = null;
+            
+            //7/16 version1: stop processing time, to check queue
+            timeStop(5000.00);
+            //7/16 version1: sample monitoredCondition
+            processedMonitoredCondtion = new MonitoredCondition(null,null);
+            processedMonitoredCondtion.SetMCID(MCstring+"__");
+
             //devide MC string to List of MEs
             //verify MEs
             //if verification success return a processedMonitoredCondition, else returns null;
@@ -76,11 +120,13 @@ namespace IASS_Test
         }
         public string AddToMCList(MonitoredCondition processedMonitoredCondtion)
         {
+            Program.AddToMCList(processedMonitoredCondtion);
             return processedMonitoredCondtion.GetMCID();
         }
         public void AddToRequestList(string pointerToMC, Request acceptedRequest)
         {
             //update the request with the pointer of MC, add the request to request list
+            Program.AddToRequestList(acceptedRequest);
         }
         public void updateTimerTable(MonitoredCondition processedMonitoredCondtion)
         {
@@ -90,6 +136,18 @@ namespace IASS_Test
                 //schedule monitored object
                 //Add to timer table
             }
+        }
+        //7/16 version 1: test code
+        public void timeStop(double pauseTime){
+            DateTime timeStart = DateTime.Now;
+            DateTime timeEnd = DateTime.Now;
+            double timeDiff = ((TimeSpan)(timeEnd - timeStart)).TotalMilliseconds;
+
+            while(timeDiff < pauseTime) {
+                timeEnd = DateTime.Now;
+                timeDiff = ((TimeSpan)(timeEnd - timeStart)).TotalMilliseconds;
+            }
+
         }
     }
 }
--- threads/RequestListener.cs
+++ threads/RequestListener.cs
@@ -14,6 +14,9 @@ namespace IASS_Test
 
         public RequestListener(EventWaitHandle doneSignal, EventWaitHandle waitProcessRequest)
         {
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs: request listener is constructed.");
+   
             this._doneSignal = doneSignal;
             this._waitProcessRequest = waitProcessRequest;
         }
@@ -23,33 +26,90 @@ namespace IASS_Test
         {
             Request newRequest = null;
             Object thisLock = new Object();
-            
+
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs: requestListener thread started.");
+
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs: start initialization.");
+            //initilization
+            // when initialization finished, set doneEvent
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs: set doneEvent[0] to say initialization finished.");
+
+            _doneSignal.Set();
+            Thread.Sleep(1000);
+
+            _doneSignal.WaitOne();
+         
             //while the program is not terminated
             while (!Program.isProgramExit)
             {
+                //@@@@@Debug message:
+                //Console.WriteLine("This is requestListener.cs: while the program is not terminated.");
+
                 //wait for new reuqest
-                newRequest = waitForNewRequest();
+                newRequest = WaitForNewRequest();
+
+                //@@@@@Debug message:
+                //Console.WriteLine("This is requestListener.cs: \n"+
+                //                  "requestID ={0}\n"+
+                //                  "request MCstring ={1}",newRequest.GetRequestUID(),newRequest.GetMCstring());
+
+
                 //add new request to request process queue
                 lock(thisLock){
                     AddNewJobToQueue(newRequest);
                 }
-                
+
+                //@@@@@Debug message:
+                //Console.WriteLine("This is requestListener.cs: a new request aded in queue");
+                //foreach (var queueItem in Program.requestProcessQueue)
+                //{
+                    //@@@@@Debug message:
+                    //Console.WriteLine("This is requestListener.cs: queue item: {0}",queueItem.GetRequestUID());
+                //}
 
                 if (Program.requestHandlerThread.ThreadState != ThreadState.Running)
                 {
+                    //@@@@@Debug message:
+                    Console.WriteLine("This is requestListener.cs: handler is sleeping, I want to wake it up");                    
                     //wake up requestHandler to process new request
                     _waitProcessRequest.Set();
                 }
                 else
                 {
+                    //@@@@@Debug message:
+                    Console.WriteLine("This is requestListener.cs: handler is running");       
                     //do nothing and keep wait the next one.
                 }
             }
+
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs: requestListener thread terminated.");
         }
-        public Request waitForNewRequest()
+        public Request WaitForNewRequest()
         {
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs,waitForNewRequest method: Pleast input request\n");
+            //#####Use hint:
+            Console.WriteLine("waiting for request input:");
+
             Request newRequest = null;
+
             //wait for new request
+            //version 1 :7/16, use readline to make assumed input
+            string tempNewRequest;
+            tempNewRequest = Console.ReadLine();
+
+            //@@@@@Debug message:
+            //Console.WriteLine("This is requestListener.cs,waitForNewRequest method :{0}",tempNewRequest);
+            //#####Use hint:
+            Console.WriteLine("received request:{0}", tempNewRequest);
+
+            //version 1 :7/16, Sample rerquest
+            newRequest = new Request(tempNewRequest, "", false, false);
+
             return newRequest;
         }
         public void AddNewJobToQueue(Request newRequest)
